5323b458fbeea16635e562facc8017b72cddb459
BhaveshWagh <bhaveshbm2711@gmail.com>
2024-08-15 10:56:41 +0530

    issue fix

--- src/components/App.js
+++ src/components/App.js
@@ -8,15 +8,14 @@ const App = () => {
     <div>
       {/* Do not remove the main div */}
       <label>
-        {" "}
-        <p> Enter your name:</p>
+        <p>Enter your name:{name && `Hello ${name}!`}</p>
         <input
           type="text"
           value={name}
           onChange={(e) => setName(e.target.value)}
-        ></input>
+        />
       </label>
-      {name !== "" && <p>Hello {name}!</p>}
+      
     </div>
   );
 };
